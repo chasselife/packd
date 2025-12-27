0edc4cb5c5fd8a0a62c7ab31b3f0dec1463dc0d0
Arnel Umandap Jr <umandap.arnel.jr@gmail.com>
2025-12-27 10:46:17 +0800

    Create SeedDataComponent in app.routes.ts for seeding dummy data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SeedDataComponent } from './components/seed-data/seed-data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'seed-data',
+    component: SeedDataComponent,
+  },
+];
